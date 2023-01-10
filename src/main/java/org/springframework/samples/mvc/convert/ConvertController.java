eac7a0a14657d7d47de0577dfc3167cae87425a1
최승은 <liberalfilm@gmail.com>
2023-01-10 20:53:59 +0900

    2022-01-10-Tue-21:53

--- src/main/java/org/springframework/samples/mvc/convert/ConvertController.java
+++ src/main/java/org/springframework/samples/mvc/convert/ConvertController.java
@@ -54,6 +54,10 @@ public class ConvertController {
 	@GetMapping("custom")
 	public String customConverter(@RequestParam @MaskFormat("###-##-###H") String value) {
 		return "Converted '" + value + "' with a custom converter";
-	}
-
+	} /** 2023-01-10-화 자현씨 테스트 결과
+	 	1> 원래 문법에 맞게 @RequestParam + 생성자(x), setter(x) => @MaskFormat 작동
+	 		원래 문법과 다르게 @RequestParam + 생성자(o), setter(o) => @MaskFormat 작동 x
+ 	 	2> 원래 문법에 맞게 @ModelAttributes + 생성자(o), setter(o) => @MaskFormat 작동
+ 	 	2> 원래 문법과 다르게 @ModelAttributes + 생성자(x), setter(x) => 되지도 않을 뿐더러 @MaskFormat 작동 x
+	 **/
 }
